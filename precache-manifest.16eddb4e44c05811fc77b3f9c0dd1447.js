ac1b0d9a2cb6c2f04e799b2d0c6a829a23fbc4ed
Greg Smelkov <gsmelkov@gmail.com>
2020-08-06 14:21:31 -0400

    Updates

---
+++ precache-manifest.16eddb4e44c05811fc77b3f9c0dd1447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8cafb59a43152bb1fae50f82fb523c",
+    "url": "/CageIMS/index.html"
+  },
+  {
+    "revision": "8cbe46bd49172167c2f8",
+    "url": "/CageIMS/static/css/main.457d6c9f.chunk.css"
+  },
+  {
+    "revision": "520ed854e6fbc3bd282d",
+    "url": "/CageIMS/static/js/2.fb1c9c0a.chunk.js"
+  },
+  {
+    "revision": "5da454b3420fa907f19676dff937ea52",
+    "url": "/CageIMS/static/js/2.fb1c9c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cbe46bd49172167c2f8",
+    "url": "/CageIMS/static/js/main.dd26bffe.chunk.js"
+  },
+  {
+    "revision": "b8201cdc5d687550d5b7",
+    "url": "/CageIMS/static/js/runtime-main.18c962d1.js"
+  }
+]);
