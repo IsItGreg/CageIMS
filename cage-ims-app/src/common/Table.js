78f04144b64d014cbb3e65b8c0ae11afc8de5ab2
seamusrioux3 <seamus.rioux3@gmail.com>
2020-08-16 19:40:20 -0400

    71 Fixed various console errors

--- cage-ims-app/src/common/Table.js
+++ cage-ims-app/src/common/Table.js
@@ -15,7 +15,6 @@ import FirstPage from "@material-ui/icons/FirstPage";
 import LastPage from "@material-ui/icons/LastPage";
 import Remove from "@material-ui/icons/Remove";
 import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
 import ViewColumn from "@material-ui/icons/ViewColumn";
 
 const tableIcons = {
