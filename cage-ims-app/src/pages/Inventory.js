6a09a50d6a955b71aa19c57153b2110755614131
seamusrioux3 <seamus.rioux3@gmail.com>
2020-08-24 14:19:01 -0400

    91 Added availability for expected return date

--- cage-ims-app/src/pages/Inventory.js
+++ cage-ims-app/src/pages/Inventory.js
@@ -76,7 +76,8 @@ class Inventory extends Component {
           title: "Expected Return Date",
           field: "expected",
           headerStyle: headerStyleGrey,
-          render: (rowData) => this.formatDate(rowData.expected),
+          render: (rowData) =>
+            rowData.expected ? this.formatDate(rowData.expected) : "Available",
           customFilterAndSearch: (term, rowData) =>
             this.formatDateForSearchBar(rowData.expected).indexOf(term) !==
               -1 || this.formatDate(rowData.expected).indexOf(term) !== -1,
