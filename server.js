f7c9ab34bdae98e0414b48a59f2a23667d38b509
Greg Smelkov <gsmelkov@gmail.com>
2020-09-16 20:47:00 -0400

    113 heroku

--- server.js
+++ server.js
@@ -80,6 +80,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use(morgan('tiny'));
 app.use('/api', routes);
 
-app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
